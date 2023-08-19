c7f40abe421641c92d86d28ba2f144638462d18e
william lee <will.lee.npc@gmail.com>
2023-08-19 09:22:05 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-/*const gifts = ["teddy bear", "drone", "doll"];
 
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    debugger;
-  }
-
-  return gifts;
-}
-
-wrapGifts(gifts);
-*/
-//const array = ["teddy bear", "drone", "doll"];
 
 function writeCards(array,event){
+    const greetings = [];
     for (let i = 0; i <= array.length-1; i++) {
         //console.log(array.length-1);
         //console.log(array[2])
-    console.log(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        greetings[i]=`Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    //return greetings;
     //debugger;
   }
-
+  //console.log(greetings);
+return greetings;
 
 }
-//writeCards(["Charlie", "Samip", "Ali"], "birthday")
+//writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
 
-//"Thank you, (Charlie), for the wonderful (birthday) gift!"
 
 function countDown(number){
     let counter = number;
